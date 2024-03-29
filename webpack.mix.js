d7e73f1363d9f356f7e7c170d8d8d63139849728
Eliran Boraks <eboraks@gmail.com>
2024-03-29 16:16:31 -0400

    Adding keys from env into manifest during build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,16 @@
 let mix = require('laravel-mix');
 const webpack = require('webpack')
 
+require('mix-env-file');
+require('laravel-mix-replace-in-file')
+
+mix.env(process.env.ENV_FILE);
+
 mix.setPublicPath('./')
     .css('assets/css/sidepanel.css', 'dist/css')
     .copy('assets/images', 'dist/images')
+    .copy('assets/sidepanel.html', 'dist')
+    .copy('assets/manifest.json', 'dist')
     .js('assets/js/background.js', 'dist/js')
     .js('assets/js/utils.js', 'dist/js')
     .js('assets/js/sidepanel.js', 'dist/js').vue()
@@ -11,7 +18,11 @@ mix.setPublicPath('./')
         processCssUrls: false
     });
 
-    
+mix.replaceInFile( {
+    files: 'dist/manifest.json',
+    from: /CHROME_EXTENSION_ID/g,
+    to: process.env.MIX_CHROME_EXTENSION_ID,
+});
 mix.webpackConfig ({
     plugins: [
     new webpack.DefinePlugin({
